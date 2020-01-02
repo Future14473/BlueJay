1cb313bfeed3c7fa7c80cb61621b01bea6437958
Benjamin Ye <BobBenBan@users.noreply.github.com>
2020-01-01 20:29:30 -0800

    Initial configuration
    Delete unused modules
    Upgrade versions
    Added kotlin to TeamCode

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Test.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Test.java
@@ -1,5 +1,12 @@
 package org.firstinspires.ftc.teamcode;
 
-public class Test {
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 
+public class Test extends LinearOpMode {
+	@Override
+	public void runOpMode() throws InterruptedException {
+		telemetry.addLine("HELLO");
+		telemetry.update();
+		Thread.sleep(10000000);
+	}
 }
